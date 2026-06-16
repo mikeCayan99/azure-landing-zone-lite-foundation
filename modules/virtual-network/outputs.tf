b887c6d2985fd7631d4e0889126fbfae4a9bb16c
Mike Cayan <mahirturan9097@gmail.com>
2026-06-16 20:52:22 +0200

    Add virtual network module

---
+++ modules/virtual-network/outputs.tf
@@ -0,0 +1,16 @@
+output "name" {
+  value = azurerm_virtual_network.virtual_network.name
+}
+
+output "location" {
+  value = azurerm_virtual_network.virtual_network.location
+}
+
+output "resource_group_name" {
+  value = azurerm_virtual_network.virtual_network.resource_group_name
+}
+
+output "address_space" {
+  value = azurerm_virtual_network.virtual_network.address_space
+}
+
